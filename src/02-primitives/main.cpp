82d3a3cbd1c3b34bbf0ee1e5150afafe8a9f24e1
Ana Nunes <anacpn2006@gmail.com>
2026-03-12 18:10:30 -0300

    Adicionar renderizacao basica de pontos.

--- src/02-primitives/main.cpp
+++ src/02-primitives/main.cpp
@@ -5,6 +5,8 @@
 #include <GLFW/glfw3.h>
 #define WINDOW_WIDTH 720
 #define WINDOW_HEIGHT 720
+#define NUM_OF_POINTS 100
+#define NUM_OF_DIM 2
 
 int main()
 {
@@ -35,10 +37,27 @@ int main()
 		return -1;
 	}
 
+	float positions[NUM_OF_POINTS*NUM_OF_DIM] = {0};
+	float x = 0;
+
+	for (int i = 0; i < (sizeof(positions)/sizeof(positions[0])); i+=2)
+	{
+		x += 0.005;
+		positions[i] = positions[i+1] = x;
+	}
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, NUM_OF_DIM, GL_FLOAT, GL_FALSE, NUM_OF_DIM * sizeof(float), 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_POINTS, 0, NUM_OF_POINTS);
 
 		glfwSwapBuffers(window);
 
