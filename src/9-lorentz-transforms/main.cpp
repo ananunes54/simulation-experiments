381ddea2a9828671ea0c260f2e835614fecfedad
Ana Nunes <anacpn2006@gmail.com>
2026-04-29 17:42:36 -0300

    Adicionar geracao dos pontos de uma reta.

--- src/9-lorentz-transforms/main.cpp
+++ src/9-lorentz-transforms/main.cpp
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 #include <matrix.h>
 #include <math.h>
+#include <cmath>
 #include <control.h>
 #include <iomanip>
 #include <utils.h>
@@ -40,8 +41,11 @@ unsigned int pointsToBeRendered(vector<3> p1, vector<3> p2, unsigned int windows
 
 	if (deltaX > deltaY)
 	{
-		pixelPos1 = std::ceil((p1.data[1]+1) * windowsize / 2);
-		pixelPos2 = std::ceil((p2.data[1]+1) * windowsize / 2);
+		std::cout << "x: " << static_cast<int>(((p1.data[1]+1.0f) * windowsize / 2.0f) + 0.5f) << std::endl;
+		std::cout << "x: " << static_cast<int>(((p2.data[1]+1.0f) * windowsize / 2.0f) + 0.5f) << std::endl;
+		pixelPos1 = static_cast<int>(((p1.data[1]+1.0f) * windowsize / 2.0f) + 0.5f);
+		pixelPos2 = static_cast<int>(((p2.data[1]+1.0f) * windowsize / 2.0f) + 0.5f);
+		std::cout << "pixelx1 = " << pixelPos1 << ", pixelx2 =" << pixelPos2 << std::endl;
 		
 		return (pixelPos1 > pixelPos2) ? pixelPos1 - pixelPos2 : pixelPos2 - pixelPos1;
 	}
@@ -55,16 +59,52 @@ unsigned int pointsToBeRendered(vector<3> p1, vector<3> p2, unsigned int windows
 	}
 }
 
+
+void generatePoints(vector<3> pos1, vector<3> pos2, unsigned int numOfPoints, double points[])
+{
+	double xIncrement, yIncrement;
+	xIncrement = (pos2.data[1] - pos1.data[1]) / (numOfPoints - 1);
+	yIncrement = (pos2.data[0] - pos1.data[0]) / (numOfPoints - 1);
+	std::cout << xIncrement << ", " << yIncrement << std::endl;
+
+	int i;
+	for (i = 0; i < 2*numOfPoints; i+=2)
+	{
+		if (i == 0)
+		{
+			points[i] = pos1.data[1];
+			points[i+1] = pos1.data[0];
+		}
+
+		
+		else
+		{
+			points[i] = points[i-2] + xIncrement;
+			points[i+1] = points[i-1] + yIncrement;
+		}
+	}
+
+	
+
+}
+
 int main()
 {
 	int transform;
 	std::cout << "0 para referencial do lab, 1 para referencial da nave: ";
 	std::cin >> transform;
 
-	vector<3> fpos1 = {0, 0.1, 1}, fpos2 = {0, 0.2, 1};
+	vector<3> fpos1 = {0.2, 0.1, 1}, fpos2 = {-0.1, 0.5, 1};
 	unsigned int numOfPoints = pointsToBeRendered(fpos1, fpos2, WINDOW_WIDTH);
-	std::cout << "pontos a serem renderizados: " << numOfPoints << numOfPoints << std::endl;
-
+	std::cout << "pontos a serem renderizados: " << numOfPoints << std::endl;
+	double objectPoints[2*numOfPoints] = {0};
+	generatePoints(fpos1, fpos2, numOfPoints, objectPoints);
+	for (int i = 0; i < 2*numOfPoints; i+=2)
+	{
+		std::cout << objectPoints[i] << ", " << objectPoints[i+1] << std::endl;
+		std::cout << i << std::endl;
+	}
+/*	
 	matrix<3, 3> omega = {
 		0, 0, 1,
 		0, 0, 0.5,
@@ -111,7 +151,7 @@ int main()
 		fpos1 = Multiply(omegaExp, fpos1);
 		fpos2 = Multiply(omegaExp, fpos2);
 	}
-
+*/
 	GLFWwindow* window;
 	int initialized = init(window, WINDOW_WIDTH, WINDOW_HEIGHT);
 
@@ -121,7 +161,7 @@ int main()
 	
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(trajectory), trajectory, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(objectPoints), objectPoints, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_DOUBLE, GL_FALSE, 2*sizeof(double), 0);
 
@@ -129,12 +169,13 @@ int main()
 	{
 
 		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawArrays(GL_LINES, 0, ITERATIONS);
+		glDrawArrays(GL_POINTS, 0, numOfPoints);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
+
 }
 
