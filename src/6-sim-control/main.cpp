b17184ce7e8621b6a320ded78961135215a5e2ed
Ana Nunes <anacpn2006@gmail.com>
2026-05-01 15:13:36 -0300

    Adicionar metodos para lidar com eventos e carregamento do glad.
    Adicionado na branch programming-tests no sub-projeto sim-control.

--- src/6-sim-control/main.cpp
+++ src/6-sim-control/main.cpp
@@ -5,10 +5,10 @@
 #include <exception>
 #define WAYLAND
 
-class GLFW
+class Glfw
 {
 public:
-	GLFW()
+	Glfw()
 	{
 		#ifdef WAYLAND
 		glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
@@ -22,23 +22,92 @@ public:
 		}
 	}
 
-	~GLFW()
+	~Glfw()
 	{
-		std::cout << "terminated" << std::endl;
+		std::cout << "glfw terminated." << std::endl;
 		glfwTerminate();
 	}
 };
+
+class Window
+{
+private:
+	GLFWwindow* handle;
+	unsigned int mWidth;
+	unsigned int mHeight;
+public:
+	Window(unsigned int width, unsigned int height)
+	{
+		#ifdef WAYLAND
+		glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+		#endif
+
+		handle = glfwCreateWindow(width, height, "window", NULL, NULL);
+
+		if (!handle)
+		{
+			const char* description;
+			int err = glfwGetError(&description);
+			throw std::runtime_error(description);
+		}
+	}
+
+	~Window()
+	{
+		std::cout << "window destroyed." << std::endl;
+		glfwDestroyWindow(handle);
+	}
+
+	void makeCurrent()
+	{
+		glfwMakeContextCurrent(handle);
+	}
+
+	int shouldClose()
+	{
+		return glfwWindowShouldClose(handle);
+	}
+
+	void swapBuffers()
+	{
+		glfwSwapBuffers(handle);
+	}
+
+};
+
+void loadGlad()
+{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::runtime_error("não foi possivel carregar o glad.");
+	}
+}
 
 
 int main ()
 {
 	try
 	{
-		GLFW glfw;
+		Glfw glfw;
+		Window window1(720, 720);
+		
+		window1.makeCurrent();
+		loadGlad();
+
+		while (!window1.shouldClose())
+		{
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			window1.swapBuffers();
+
+			glfwPollEvents();
+		}
 	}
+
 	catch (std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
 	}
+
 	return 0;
 }
