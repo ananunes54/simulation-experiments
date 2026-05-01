184d8e4fbbfaab0581cb4a206ac5ac71aa3715f0
Ana Nunes <anacpn2006@gmail.com>
2026-05-01 10:53:17 -0300

    Testar funcinamento de exceptions.

--- src/6-sim-control/main.cpp
+++ src/6-sim-control/main.cpp
@@ -1,45 +1,37 @@
 #include <iostream>
-#include <matrix.h>
+#include <exception>
 
-namespace simulation
+class teste
 {
-	struct state
-	{	
-		vector<3> linAcceleration = {0, 0, 0};
-		vector<3> angAcceleration = {0, 0, 0};
-		vector<3> spacialVelocity = {0, 0, 0};
-		vector<3> spacialPosition = {0, 0, 0};
-		char visibleAxis1;
-		char visibleAxis2;
-	};
-
-	void SetInitialValues(simulation::state& state)
+public:
+	teste()
 	{
-		std::cout << "aceleracao linear: ";
-		std::cin >> state.linAcceleration.data[0] >> state.linAcceleration.data[1] >> state.linAcceleration.data[2];
-		std::cout << "aceleracao angular: ";
-		std::cin >> state.angAcceleration.data[0] >> state.angAcceleration.data[1] >> state.angAcceleration.data[2];
-		std::cout << "velocidade: ";
-		std::cin >> state.spacialVelocity.data[0] >> state.spacialVelocity.data[1] >> state.spacialVelocity.data[2];
-		std::cout << "posicao: ";
-		std::cin >> state.spacialPosition.data[0] >> state.spacialPosition.data[1] >> state.spacialPosition.data[2];
-		std::cout << "eixos visiveis: ";
-		std::cin >> state.visibleAxis1 >> state.visibleAxis2;
+		std::cout << "objeto criado" << std::endl;
 	}
-}
 
-int main()
+	void funcao()
+	{
+		throw std::runtime_error("erro ao criar objeto");
+	}
+
+	~teste()
+	{
+		std::cout << "objeto destruido" << std::endl;
+	}
+};
+
+
+int main ()
 {
-	simulation::state state;
+	try
+	{
+		teste a;
+		a.funcao();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "exceção encontrada: " << e.what() << std::endl;
+	}
 
-	simulation::SetInitialValues(state);
-
-	Print(state.linAcceleration);
-	Print(state.angAcceleration);
-	Print(state.spacialVelocity);
-	Print(state.spacialPosition);
-
-	std::cout << state.visibleAxis1 << std::endl;
-	std::cout << state.visibleAxis2 << std::endl;
-
+	return 0;
 }
