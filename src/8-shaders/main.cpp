13526e3ca0c7f027f493a67f40ecf47643268955
Ana Nunes <anacpn2006@gmail.com>
2026-05-04 18:42:34 -0300

    Adicionar element buffer para desenhar um quadrado.
    Adicionado na branch programming-tests no sub-projeto de shaders.

--- src/8-shaders/main.cpp
+++ src/8-shaders/main.cpp
@@ -3,76 +3,70 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#define WINDOW_WIDTH 720
-#define WINDOW_HEIGHT 720
+#include <utils.h>
+#include <exception>
 
 int main()
 {
-	glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
-	glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
-
-	if (!glfwInit())
+	try 
 	{
-		return -1;
+		Glfw glfw;
+		unsigned windowWidth = 800, windowHeight = 800;
+		Window window(windowWidth, windowHeight);
+		window.makeCurrent();
+
+		loadGlad();
+
+		float squarePositions[18] = {
+			-0.5f, -0.5f,
+			0.5f, -0.5f,
+			0.5f, 0.5f,
+			-0.5f, 0.5f,
+		};
+
+		unsigned int squareIndices[6] = {
+			0, 1, 2,
+			2, 3, 0
+		};
+		
+
+		unsigned int vao;
+		glGenBuffers(1, &vao);
+
+		unsigned int vbo;
+		glGenBuffers(1, &vbo);
+
+		unsigned int ebo;
+		glGenBuffers(1, &ebo);
+
+		glBindVertexArray(vao);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(squarePositions), squarePositions, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIndices), squareIndices, GL_STATIC_DRAW);
+		
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+		
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+		while (!window.shouldClose())
+		{
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, squareIndices);
+
+			window.swapBuffers();
+
+			glfwPollEvents();
+		}
 	}
-
-	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "window", NULL, NULL);
-
-	if (!window)
+	catch(std::exception& e)
 	{
-		std::cout << "erro ao criar janela." << std::endl;
-		glfwTerminate();
-		return -1;
+		std::cout << "exception caught: " << e.what() << std::endl;
 	}
-
-	glfwMakeContextCurrent(window);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "erro ao carregar glad." << std::endl;
-		glfwDestroyWindow(window);
-		glfwTerminate();
-		return -1;
-	}
-
-
-	float positions[6] = {
-		-0.5f, -0.5f,
-		0.5f, -0.5f,
-		0, 0.5f 
-	};
-	
-
-	unsigned int VAO;
-	glGenBuffers(1, &VAO);
-
-	unsigned int buffer;
-	glGenBuffers(1, &buffer);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-	
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_DOUBLE, GL_FALSE, 2*sizeof(float), 0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-
-		glfwPollEvents();
-	}
-
-
-	glfwDestroyWindow(window);
-	glfwTerminate();
 
 	return 0;
 }
