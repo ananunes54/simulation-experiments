8bb654ef0349f22280ded13223ce29aa15ec0b0e
Ana Nunes <anacpn2006@gmail.com>
2026-05-14 19:48:55 -0300

    Adicionar prototipo de classe para objetos da simulacao.

---
+++ include/object.h
@@ -0,0 +1,66 @@
+#ifndef OBJECT_H_INCLUDED
+#define OBJECT_H_INCLUDED
+
+#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <math.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <vector>
+
+class Object
+{
+private:
+	std::vector<float> m_vertices;
+	std::vector<unsigned int> m_indices;
+	unsigned int m_vao;
+	unsigned int m_vbo;
+	unsigned int m_ebo;
+
+
+public:
+	Object(std::vector<float>& vertices, std::vector<unsigned int>& indices) : m_vertices(vertices), m_indices(indices)
+	{
+		glGenBuffers(1, &m_vao);
+		glGenBuffers(1, &m_vbo);
+		glGenBuffers(1, &m_ebo);
+
+		glBindVertexArray(m_vao);
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+
+		glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(float), m_vertices.data(), GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_indices.size() * sizeof(unsigned int), m_indices.data(), GL_STATIC_DRAW);
+
+		glBindVertexArray(0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	void setAttribute(unsigned int location, unsigned int numOfComponents,  unsigned int strideInBytes, unsigned int offset)
+	{
+		glBindVertexArray(m_vao);
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+
+		glVertexAttribPointer(location, numOfComponents, GL_FLOAT, GL_FALSE, strideInBytes, reinterpret_cast<void*>(static_cast<uintptr_t>(offset)));
+		glEnableVertexAttribArray(0);
+
+		glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	void draw()
+	{
+		glBindVertexArray(m_vao);
+		glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, m_indices.data());
+		glBindVertexArray(0);
+	}
+
+	
+
+};
+
+#endif
--- src/8-shaders/main.cpp
+++ src/8-shaders/main.cpp
@@ -10,6 +10,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <matrix.h>
+#include <object.h>
 
 int main()
 {
@@ -22,12 +23,19 @@ int main()
 
 		loadGlad();
 
-		float squarePositions[8] = {
-			-0.5f / 2.0, -0.5f / 2.0f,
-			0.5f / 2.0f, -0.5f / 2.0f,
-			0.5f / 2.0f, 0.5f / 2.0f,
-			-0.5f / 2.0f, 0.5f / 2.0f
+		std::vector<float> vertices{
+			-0.5f / 2.0,  -0.5f / 2.0f,
+			 0.5f / 2.0f, -0.5f / 2.0f,
+			 0.5f / 2.0f,  0.5f / 2.0f,
+			-0.5f / 2.0f,  0.5f / 2.0f
+
 		};
+
+		std::vector<unsigned int> indices{
+				0, 1, 2,
+				2, 3, 0
+		};
+
 
 		matrix<3, 3> aMat = {0.0f, 0.0f, 0.005f, 
 				  0.0f, 0.0f, 0.0f,
@@ -43,33 +51,10 @@ int main()
 					omegaMat.data[1], omegaMat.data[4], omegaMat.data[7],
 					omegaMat.data[2], omegaMat.data[5], omegaMat.data[8]);
 
-		unsigned int squareIndices[6] = {
-			0, 1, 2,
-			2, 3, 0
-		};
-		
 
-		unsigned int vao;
-		glGenBuffers(1, &vao);
+		Object obj(vertices, indices);
+		obj.setAttribute(0, 2, 2*sizeof(float), 0);	
 
-		unsigned int vbo;
-		glGenBuffers(1, &vbo);
-
-		unsigned int ebo;
-		glGenBuffers(1, &ebo);
-
-		glBindVertexArray(vao);
-
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(squarePositions), squarePositions, GL_STATIC_DRAW);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIndices), squareIndices, GL_STATIC_DRAW);
-		
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
-		
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 		std::string vertexShaderPath("/home/ana/sim-experiments/src/8-shaders/default.vert");
 		std::string vertexShaderStr = readFromFile(vertexShaderPath);
@@ -104,7 +89,7 @@ int main()
 
 			lorentzMat = lorentzMat * auxMat;
 
-			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, squareIndices);
+			obj.draw();
 
 			window.swapBuffers();
 
