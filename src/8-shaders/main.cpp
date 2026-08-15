c2243af68a180457a1c8dfd8064a0a8aaadc3c8b
Ana Nunes <anacpn2006@gmail.com>
2026-08-15 19:04:25 -0300

    Adicionar controles para a matriz de view.

--- src/8-shaders/main.cpp
+++ src/8-shaders/main.cpp
@@ -25,6 +25,7 @@
 
 void initialize(glm::mat4& modelMat, glm::mat4& viewMat, glm::mat4& projectionMat, Window& window);
 glm::mat4 setModelMat(glm::vec3& translation, glm::vec3& rotation, float scale);
+glm::mat4 setViewMat(glm::vec3& position, glm::vec3& target, glm::vec3& up);
 
 Mat5 buildPoincare(const glm::vec3& linAcceleration, const glm::vec3& angAcceleration, const glm::vec4& translation)
 {
@@ -62,9 +63,14 @@ int main()
         glm::vec3 objectRotation(0.0f);
         float objectScale(1.0f);
 
+        glm::vec3 viewPosition(0.0f);
+        glm::vec3 viewTarget(0.0f);
+        glm::vec3 viewUp(0.0f, 1.0f, 0.0f);
+
         bool isPaused = true;
         bool matrixAltered = false;
         bool objectAltered = true;
+        bool viewAltered = true;
 
 		float time = 0.0f;
 		float properTime = 0.0f;
@@ -200,6 +206,34 @@ int main()
                 objectAltered = true;
             }
 
+
+
+            if (ImGui::Button("Reset4"))
+            {
+                viewAltered = true;
+                viewPosition = glm::vec3(0.0f);
+            }
+
+            ImGui::SameLine();
+
+            if (ImGui::SliderFloat3("Posição camera", glm::value_ptr(viewPosition), -50.0f, 50.0f, "%.0f"))
+            {
+                viewAltered = true;
+            }
+
+            if (ImGui::Button("Reset5"))
+            {
+                viewAltered = true;
+                viewTarget = glm::vec3(0.0f);
+            }
+
+            ImGui::SameLine();
+
+            if (ImGui::SliderFloat3("Alvo camera", glm::value_ptr(viewTarget), -50.0f, 50.0f, "%.0f"))
+            {
+                viewAltered = true;
+            }
+
             ImGui::End();
             ///////////////
 
@@ -208,6 +242,13 @@ int main()
                 modelMat = setModelMat(objectPosition, objectRotation, objectScale);
                 material.setGlmMat4("u_modelMat", modelMat);
                 objectAltered = false;
+            }
+
+            if (viewAltered)
+            {
+                viewMat = setViewMat(viewPosition, viewTarget, viewUp);
+                material.setGlmMat4("u_viewMat", viewMat);
+                viewAltered = false;
             }
 
 
@@ -260,6 +301,11 @@ glm::mat4 setModelMat(glm::vec3& translation, glm::vec3& rotation, float scale)
     modelMat = glm::rotate(modelMat, glm::radians(rotation[2]), glm::vec3(0.0f, 0.0f, 2.0f));
     modelMat = glm::scale(modelMat, glm::vec3(scale, scale, scale));
     return modelMat;
+}
+
+glm::mat4 setViewMat(glm::vec3& position, glm::vec3& target, glm::vec3& up)
+{
+    return glm::lookAt(position, target, up);
 }
 
 
