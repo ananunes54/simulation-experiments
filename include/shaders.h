a8d9144cbb4bf5c923737ff1b4e1cfea680fa35a
Ana Nunes <anacpn2006@gmail.com>
2026-08-28 08:43:33 -0300

    [WIP] passar abstracao dos uniforms para classe shader. Limpar classe shader.

--- include/shaders.h
+++ include/shaders.h
@@ -5,13 +5,6 @@
 #include <string>
 #include <vector>
 
-struct UniformInfo
-{
-    unsigned int program;
-    unsigned int index;
-    std::string name;
-    GLenum type;
-};
 
 
 class Shader
@@ -20,20 +13,15 @@ class Shader
         std::string m_vertexShaderPath;
         std::string m_fragmentShaderPath;
         unsigned int m_ID;
-        int m_uniformsCount;
-        std::vector<UniformInfo> m_uniforms;
 
     private:
         unsigned int compileShader(unsigned int shaderType, std::string& shaderSource);
         unsigned int createProgram();
-        void introspectProgram();
 
     public:
         Shader(std::string& vertexShaderPath, std::string& fragmentShaderPath);
         ~Shader();
         unsigned int getID();
-        int getUniformsCount();
-        std::vector<UniformInfo> getUniforms();
 };
 
 #endif 
--- shared/shaders.cpp
+++ shared/shaders.cpp
@@ -11,7 +11,6 @@
 Shader::Shader(std::string& vertexShaderPath, std::string& fragmentShaderPath) : m_vertexShaderPath(vertexShaderPath), m_fragmentShaderPath(fragmentShaderPath) 
 {
     m_ID = Shader::createProgram();
-    introspectProgram();
 }
 
 Shader::~Shader()
@@ -127,36 +126,8 @@ unsigned int Shader::createProgram()
 
 }
 
-void Shader::introspectProgram()
-{
-    GLCall(glGetProgramiv(m_ID, GL_ACTIVE_UNIFORMS, &m_uniformsCount));
-    m_uniforms.resize(m_uniformsCount);
-
-    int maxNameLenght;
-    GLCall(glGetProgramiv(m_ID, GL_ACTIVE_UNIFORM_MAX_LENGTH, &maxNameLenght));
-
-    GLint size;
-
-    for (auto i = 0; i < m_uniformsCount; i++)
-    {
-        m_uniforms[i].program = m_ID;
-        m_uniforms[i].index = i;
-        m_uniforms[i].name.assign(maxNameLenght, '\0');
-        GLCall(glGetActiveUniform(m_ID, i, maxNameLenght, nullptr, &size, &(m_uniforms[i]).type, m_uniforms[i].name.data()));
-    }
-}
-
 unsigned int Shader::getID()
 {
     return m_ID;
 }
 
-int Shader::getUniformsCount()
-{
-    return m_uniformsCount;
-}
-
-std::vector<UniformInfo> Shader::getUniforms()
-{
-    return m_uniforms;
-}
